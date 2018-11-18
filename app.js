8c93b76562afc86b674bf9084a4959fd0a3673e1
Redrogue12 <exgc12@gmail.com>
2018-11-18 01:24:31 -0400

    set main endpoint

--- app.js
+++ app.js
@@ -9,3 +9,12 @@ app.listen(port);
 app.use('/assets', express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
+
+app.use('/', function (req, res, next) {
+  console.log('Request URL: ' + req.url);
+  next();
+});
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
